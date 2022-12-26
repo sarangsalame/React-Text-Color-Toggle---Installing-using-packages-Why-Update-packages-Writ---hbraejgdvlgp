0f97252266c78f68829a5d770737decaa027268d
sarang salame <115799065+sarangsalame@users.noreply.github.com>
2022-12-26 22:50:29 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ const App = () => {
 //code here 
   return (
     <div id="main">
-      <p className={css?redColor:blueColor} >Newton School</p>
+      <p className={css?"redColor":"blueColor"} >Newton School</p>
       <button id='button' onClick={change}>Change Style</button>
     </div>
   )
